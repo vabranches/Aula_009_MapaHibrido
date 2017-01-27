0ab5cb003725e8bdee363a8b211357bd93519f66
Swift <swift@WozMac8.local>
2017-01-27 21:00:32 -0200

    inicial

--- Aula_009_MapaHibrido/ViewController.swift
+++ Aula_009_MapaHibrido/ViewController.swift
@@ -1,25 +1,79 @@
-//
-//  ViewController.swift
-//  Aula_009_MapaHibrido
-//
-//  Created by Swift on 27/01/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var mapa: MKMapView!
+    
+    
+    //MARK: Propriedades
+    var gerenciadorGPS = CLLocationManager()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Configurando a precisão a ser utilizada no GPS
+        gerenciadorGPS.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //Determinando a regiao a ser inicializada no mapa
+        //Ibirapuera: -23.587082,-46.6611527
+        let coordenadasIbira = CLLocationCoordinate2DMake(-23.587082, -46.6611527)
+        let zoomMapa = MKCoordinateSpanMake(0.02, 0.02)
+        let regiaoMapa = MKCoordinateRegionMake(coordenadasIbira, zoomMapa)
+        mapa.setRegion(regiaoMapa, animated: true)
+        mapa.setCenter(coordenadasIbira, animated: true)
+        
+        
+        //Criando um Pino para a localização
+        let pinoIbira = MKPointAnnotation()
+        pinoIbira.title = "Parque do Ibirapuera"
+        pinoIbira.subtitle = "Chegamos ao Parque"
+        pinoIbira.coordinate = coordenadasIbira
+        
+        mapa.addAnnotation(pinoIbira)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func atualizar(_ sender: UIBarButtonItem) {
+        if CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse && CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedAlways{
+            
+            gerenciadorGPS.requestWhenInUseAuthorization()
+        } else {
+            mapa.showsUserLocation = true
+            gerenciadorGPS.delegate = self
+            gerenciadorGPS.startUpdatingLocation()
+        }
     }
+    
+    @IBAction func mudarMapa(_ sender: UISegmentedControl) {
+        
+        let indice = sender.selectedSegmentIndex
+        switch indice {
+        case 0:
+            mapa.mapType = MKMapType.standard
+        case 1:
+            mapa.mapType = MKMapType.satellite
+        case 2:
+            mapa.mapType = MKMapType.hybrid
+        default:
+            mapa.mapType = MKMapType.standard
+        }
+    }
+    
+    //MARK: Metodos de CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let localizacao = locations.last
+        let zoom = MKCoordinateSpanMake(0.01, 0.01)
+        let regiao = MKCoordinateRegionMake(localizacao!.coordinate, zoom)
+        mapa.setRegion(regiao, animated: true)
 
+    }
 
 }
 
